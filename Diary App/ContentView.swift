89c2cc610b3e897430709269f9e51ff9543b7ac3
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-12-06 09:42:57 +0900

    Initial Commit

---
+++ Diary App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diary App
+//
+//  Created by 志水拓哉 on 2025/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diary App/Diary_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diary_AppApp.swift
+//  Diary App
+//
+//  Created by 志水拓哉 on 2025/12/06.
+//
+
+import SwiftUI
+
+@main
+struct Diary_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
